5f83748244bd003edd6bede19f203f40e59a5800
wodjafink <wodjafink@gmail.com>
2018-03-09 19:02:01 -0800

    Game now stops when you guess 13 letters.  It is easy to check this when I enter non letter characters such as numbers, so now need to think about how to do deal with that case

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -31,33 +31,64 @@ function updateDisplayWord(){
 	}
 }
 
+// This will return true if all the entries of ourWordTable are true
+function checkWin()
+{
+	for(var i = 0; i < ourWordTable.length; i++){
+		if( !(ourWordTable[i]))
+			return false
+	}
+	console.log('A WINNER IS YOU')
+	return true
+}
+
+// Am I using functions right
+function updateHTML()
+{
+	document.getElementById("guess-word").innerHTML = displayWord.join(" ");
+	document.getElementById("num-user-guesses").innerHTML = allowedGuesses;
+	document.getElementById("guessed-letters").innerHTML = badLetters.join(" ");
+}
+
+// This is pretty sweet because it'll run when the page loads.  So I can set up the
+// 'dynamic HTML' or whatever right away rather than waiting for the first characters to be pressed
+window.onload = function(event)
+{
+	updateHTML();
+}
+
 document.onkeyup = function(event) {
     var guessKey = event.key;
 
     console.log("got key " + guessKey + " index is " + guessWord.indexOf(guessKey));
 
-    if (guessWord.indexOf(guessKey) > -1)
+    if ((allowedGuesses > 0) && !(checkWin()))
     {
-    	for (var i = 0; i < guessWord.length; i++)
-    	{
-    		if (guessWord[i] === guessKey)
-    		{
-    			ourWordTable[i] = true;
-    		}
-    	}
-    }
-    else {
-    	if (badLetters.indexOf(guessKey) === -1)
-    	{
-    		badLetters.push(guessKey);
-    		allowedGuesses--;
-    	}
-    }
-
-    updateDisplayWord();
-
-    document.getElementById("guess-word").innerHTML = displayWord.join(" ");
-    document.getElementById("num-user-guesses").innerHTML = allowedGuesses;
-    document.getElementById("guessed-letters").innerHTML = badLetters.join(" ");
+	    if (guessWord.indexOf(guessKey) > -1)
+	    {
+	    	for (var i = 0; i < guessWord.length; i++)
+	    	{
+	    		if (guessWord[i] === guessKey)
+	    		{
+	    			ourWordTable[i] = true;
+	    		}
+	    	}
+	    }
+	    else {
+	    	if (badLetters.indexOf(guessKey) === -1)
+	    	{
+	    		badLetters.push(guessKey);
+	    		allowedGuesses--;
+	    	}
+	    }
+	    updateDisplayWord();
+    	updateHTML();
+	}
+	else if (allowedGuesses === 0)
+	{
+		//Have to update one more time to show that the guesses actually reaches 0
+	    updateDisplayWord();
+    	updateHTML();
+	}
 }
 
